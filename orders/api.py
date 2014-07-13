0ba051fd20b0e998ef5c992b61949520efa0a90f
Miguel Grinberg <miguelgrinberg50@gmail.com>
2014-07-13 00:22:53 -0700

    products, orders and items (v0.2)

--- orders/api.py
+++ orders/api.py
@@ -1,6 +1,11 @@
 import os
+from datetime import datetime
+from dateutil import parser as datetime_parser
+from dateutil.tz import tzutc
 from flask import Flask, url_for, jsonify, request
 from flask.ext.sqlalchemy import SQLAlchemy
+from utils import split_url
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 db_path = os.path.join(basedir, '../data.sqlite')
@@ -19,9 +24,35 @@ class Customer(db.Model):
     __tablename__ = 'customers'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True)
+    orders = db.relationship('Order', backref='customer', lazy='dynamic')
 
     def get_url(self):
         return url_for('get_customer', id=self.id, _external=True)
+
+    def export_data(self):
+        return {
+            'self_url': self.get_url(),
+            'name': self.name,
+            'orders_url': url_for('get_customer_orders', id=self.id,
+                                  _external=True)
+        }
+
+    def import_data(self, data):
+        try:
+            self.name = data['name']
+        except KeyError as e:
+            raise ValidationError('Invalid customer: missing ' + e.args[0])
+        return self
+
+
+class Product(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True)
+    items = db.relationship('Item', backref='product', lazy='dynamic')
+
+    def get_url(self):
+        return url_for('get_product', id=self.id, _external=True)
 
     def export_data(self):
         return {
@@ -33,7 +64,72 @@ class Customer(db.Model):
         try:
             self.name = data['name']
         except KeyError as e:
-            raise ValidationError('Invalid customer: missing ' + e.args[0])
+            raise ValidationError('Invalid product: missing ' + e.args[0])
+        return self
+
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+    id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'),
+                            index=True)
+    date = db.Column(db.DateTime, default=datetime.now)
+    items = db.relationship('Item', backref='order', lazy='dynamic',
+                            cascade='all, delete-orphan')
+
+    def get_url(self):
+        return url_for('get_order', id=self.id, _external=True)
+
+    def export_data(self):
+        return {
+            'self_url': self.get_url(),
+            'customer_url': self.customer.get_url(),
+            'date': self.date.isoformat() + 'Z',
+            'items_url': url_for('get_order_items', id=self.id,
+                                 _external=True)
+        }
+
+    def import_data(self, data):
+        try:
+            self.date = datetime_parser.parse(data['date']).astimezone(
+                tzutc()).replace(tzinfo=None)
+        except KeyError as e:
+            raise ValidationError('Invalid order: missing ' + e.args[0])
+        return self
+
+
+class Item(db.Model):
+    __tablename__ = 'items'
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'), index=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'),
+                           index=True)
+    quantity = db.Column(db.Integer)
+
+    def get_url(self):
+        return url_for('get_item', id=self.id, _external=True)
+
+    def export_data(self):
+        return {
+            'self_url': self.get_url(),
+            'order_url': self.order.get_url(),
+            'product_url': self.product.get_url(),
+            'quantity': self.quantity
+        }
+
+    def import_data(self, data):
+        try:
+            endpoint, args = split_url(data['product_url'])
+            self.quantity = int(data['quantity'])
+        except KeyError as e:
+            raise ValidationError('Invalid order: missing ' + e.args[0])
+        if endpoint != 'get_product' or not 'id' in args:
+            raise ValidationError('Invalid product URL: ' +
+                                  data['product_url'])
+        self.product = Product.query.get(args['id'])
+        if self.product is None:
+            raise ValidationError('Invalid product URL: ' +
+                                  data['product_url'])
         return self
 
 
@@ -62,6 +158,105 @@ def edit_customer(id):
     db.session.commit()
     return jsonify({})
 
+
+@app.route('/products/', methods=['GET'])
+def get_products():
+    return jsonify({'products': [product.get_url() for product in
+                                 Product.query.all()]})
+
+@app.route('/products/<int:id>', methods=['GET'])
+def get_product(id):
+    return jsonify(Product.query.get_or_404(id).export_data())
+
+@app.route('/products/', methods=['POST'])
+def new_product():
+    product = Product()
+    product.import_data(request.json)
+    db.session.add(product)
+    db.session.commit()
+    return jsonify({}), 201, {'Location': product.get_url()}
+
+@app.route('/products/<int:id>', methods=['PUT'])
+def edit_product(id):
+    product = Product.query.get_or_404(id)
+    product.import_data(request.json)
+    db.session.add(product)
+    db.session.commit()
+    return jsonify({})
+
+
+
+@app.route('/orders/', methods=['GET'])
+def get_orders():
+    return jsonify({'orders': [order.get_url() for order in Order.query.all()]})
+
+@app.route('/customers/<int:id>/orders/', methods=['GET'])
+def get_customer_orders(id):
+    customer = Customer.query.get_or_404(id)
+    return jsonify({'orders': [order.get_url() for order in
+                               customer.orders.all()]})
+
+@app.route('/orders/<int:id>', methods=['GET'])
+def get_order(id):
+    return jsonify(Order.query.get_or_404(id).export_data())
+
+@app.route('/customers/<int:id>/orders/', methods=['POST'])
+def new_customer_order(id):
+    customer = Customer.query.get_or_404(id)
+    order = Order(customer=customer)
+    order.import_data(request.json)
+    db.session.add(order)
+    db.session.commit()
+    return jsonify({}), 201, {'Location': order.get_url()}
+
+@app.route('/orders/<int:id>', methods=['PUT'])
+def edit_order(id):
+    order = Order.query.get_or_404(id)
+    order.import_data(request.json)
+    db.session.add(order)
+    db.session.commit()
+    return jsonify({})
+
+@app.route('/orders/<int:id>', methods=['DELETE'])
+def delete_order(id):
+    order = Order.query.get_or_404(id)
+    db.session.delete(order)
+    db.session.commit()
+    return jsonify({})
+
+
+@app.route('/orders/<int:id>/items/', methods=['GET'])
+def get_order_items(id):
+    order = Order.query.get_or_404(id)
+    return jsonify({'items': [item.get_url() for item in order.items.all()]})
+
+@app.route('/items/<int:id>', methods=['GET'])
+def get_item(id):
+    return jsonify(Item.query.get_or_404(id).export_data())
+
+@app.route('/orders/<int:id>/items/', methods=['POST'])
+def new_order_item(id):
+    order = Order.query.get_or_404(id)
+    item = Item(order=order)
+    item.import_data(request.json)
+    db.session.add(item)
+    db.session.commit()
+    return jsonify({}), 201, {'Location': item.get_url()}
+
+@app.route('/items/<int:id>', methods=['PUT'])
+def edit_item(id):
+    item = Item.query.get_or_404(id)
+    item.import_data(request.json)
+    db.session.add(item)
+    db.session.commit()
+    return jsonify({})
+
+@app.route('/items/<int:id>', methods=['DELETE'])
+def delete_item(id):
+    item = Item.query.get_or_404(id)
+    db.session.delete(item)
+    db.session.commit()
+    return jsonify({})
 
 if __name__ == '__main__':
     db.create_all()
---
+++ orders/utils.py
@@ -0,0 +1,33 @@
+from flask.globals import _app_ctx_stack, _request_ctx_stack
+from werkzeug.urls import url_parse
+from werkzeug.exceptions import NotFound
+
+
+def split_url(url, method='GET'):
+    """Returns the endpoint name and arguments that match a given URL. In
+    other words, this is the reverse of Flask's url_for()."""
+    appctx = _app_ctx_stack.top
+    reqctx = _request_ctx_stack.top
+    if appctx is None:
+        raise RuntimeError('Attempted to match a URL without the '
+                           'application context being pushed. This has to be '
+                           'executed when application context is available.')
+
+    if reqctx is not None:
+        url_adapter = reqctx.url_adapter
+    else:
+        url_adapter = appctx.url_adapter
+        if url_adapter is None:
+            raise RuntimeError('Application was not able to create a URL '
+                               'adapter for request independent URL matching. '
+                               'You might be able to fix this by setting '
+                               'the SERVER_NAME config variable.')
+    parsed_url = url_parse(url)
+    if parsed_url.netloc is not '' and \
+                    parsed_url.netloc != url_adapter.server_name:
+        raise ValidationError('Invalid URL: ' + url)
+    try:
+        result = url_adapter.match(parsed_url.path, method)
+    except NotFound:
+        raise ValidationError('Invalid URL: ' + url)
+    return result
