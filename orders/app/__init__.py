20e718ea680ba19e1455eca6a3b4fcd2c6e2837c
Miguel Grinberg <miguelgrinberg50@gmail.com>
2014-07-19 23:41:58 -0700

    rate limiting (v0.15)

--- orders/app/__init__.py
+++ orders/app/__init__.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, jsonify, g
 from flask.ext.sqlalchemy import SQLAlchemy
-from .decorators import json, no_cache
+from .decorators import json, no_cache, rate_limit
 
 db = SQLAlchemy()
 
@@ -21,10 +21,18 @@ def create_app(config_name):
     from .api_v1 import api as api_blueprint
     app.register_blueprint(api_blueprint, url_prefix='/api/v1')
 
+    # register an after request handler
+    @app.after_request
+    def after_request(rv):
+        headers = getattr(g, 'headers', {})
+        rv.headers.extend(headers)
+        return rv
+
     # authentication token route
     from .auth import auth
     @app.route('/get-auth-token')
     @auth.login_required
+    @rate_limit(1, 600)  # one call per 10 minute period
     @no_cache
     @json
     def get_auth_token():
--- orders/app/api_v1/__init__.py
+++ orders/app/api_v1/__init__.py
@@ -1,11 +1,12 @@
 from flask import Blueprint
 from ..auth import auth_token
-from ..decorators import etag
+from ..decorators import etag, rate_limit
 
 api = Blueprint('api', __name__)
 
 
 @api.before_request
+@rate_limit(limit=5, period=15)
 @auth_token.login_required
 def before_request():
     """All routes in this blueprint require authentication."""
