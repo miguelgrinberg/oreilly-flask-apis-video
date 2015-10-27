d7b43fbb2aeb81a80546099079cdfb200a1cffcc
Josh Friend <jfriend@michiganlabs.com>
2015-10-27 10:27:49 -0400

    Improved etag handling
    * Allows wrapping an endpoint that also supports PUT/POST/DELETE
    * Use Werkzeug utilities for etag parsing/generation

--- orders/app/decorators/caching.py
+++ orders/app/decorators/caching.py
@@ -1,6 +1,8 @@
 import functools
 import hashlib
 from flask import request, make_response, jsonify
+from werkzeug.http import generate_etag, HTTP_STATUS_CODES
+from werkzeug.datastructures import ETag
 
 
 def cache_control(*directives):
@@ -31,48 +33,47 @@ def etag(f):
     """Add entity tag (etag) handling to the decorated route."""
     @functools.wraps(f)
     def wrapped(*args, **kwargs):
-        # etags only make sense for request that are cacheable, so only
-        # GET and HEAD requests are allowed
-        assert request.method in ['GET', 'HEAD'],\
-            '@etag is only supported on GET and HEAD requests'
-
         # invoke the wrapped function and generate a response object from
         # its result
         rv = f(*args, **kwargs)
-        rv = make_response(rv)
+        response = make_response(rv)
+
+        # etags only make sense for request that are cacheable, so only
+        # GET and HEAD requests are processed
+        if request.method not in ['GET', 'HEAD']:
+            return response
 
         # if the response is not a code 200 OK then we let it through
         # unchanged
-        if rv.status_code != 200:
-            return rv
+        if response.status_code != 200:
+            return response
 
         # compute the etag for this request as the MD5 hash of the response
         # text and set it in the response header
-        etag = '"' + hashlib.md5(rv.get_data()).hexdigest() + '"'
-        rv.headers['ETag'] = etag
+        etag = generate_etag(response.get_data())
+        response.set_etag(etag)
 
-        # handle If-Match and If-None-Match request headers if present
-        if_match = request.headers.get('If-Match')
-        if_none_match = request.headers.get('If-None-Match')
-        if if_match:
-            # only return the response if the etag for this request matches
+        status = 200
+        # Handle If-Match and If-None-Match request headers if present
+        if request.if_match:
+            # Only return the response if the etag for this request matches
             # any of the etags given in the If-Match header. If there is no
             # match, then return a 412 Precondition Failed status code
-            etag_list = [tag.strip() for tag in if_match.split(',')]
-            if etag not in etag_list and '*' not in etag_list:
-                response = jsonify({'status': 412, 'error': 'precondition failed',
-                                    'message': 'precondition failed'})
-                response.status_code = 412
-                return response
-        elif if_none_match:
-            # only return the response if the etag for this request does not
+            if not etag in request.if_match:
+                status = 412
+        elif request.if_none_match:
+            # Only return the response if the etag for this request does not
             # match any of the etags given in the If-None-Match header. If
             # one matches, then return a 304 Not Modified status code
-            etag_list = [tag.strip() for tag in if_none_match.split(',')]
-            if etag in etag_list or '*' in etag_list:
-                response = jsonify({'status': 304, 'error': 'not modified',
-                                    'message': 'resource not modified'})
-                response.status_code = 304
-                return response
-        return rv
+            if etag in request.if_none_match:
+                status = 304
+
+        # check if response needs to be modified due to ETags
+        if status != 200:
+            message = HTTP_STATUS_CODES[status]
+            response = jsonify({'status': status, 'error': message,
+                                'message': message})
+            response.status_code = status
+
+        return response
     return wrapped
